37ffaf8f11f3a3d2c3a8ee6d5b23823ae02762c4
104sama <89830007+sdax04@users.noreply.github.com>
2025-04-24 17:51:08 +0800

    Update custom_chat.cpp

--- src/chimera/custom_chat/custom_chat.cpp
+++ src/chimera/custom_chat/custom_chat.cpp
@@ -624,8 +624,7 @@ extern const std::string& get_console_text_temp();
 	    console_output("chat_input_buffer.c_str() is:%s",chat_input_buffer.c_str());
 	    chat_out(0, chat_input_temp.c_str());
 	    chat_out(0, chat_input_temp);
-	    chat_out(0, u16_to_u8(chat_input_temp));
-	    chat_out(0, u8_to_u16(chat_input_temp));
+	  
         enable_input(false);    
     }
 
