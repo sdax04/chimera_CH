3dbc0736089b367e248b6fe97dc4bfc667b094c3
104sama <89830007+sdax04@users.noreply.github.com>
2025-04-24 16:58:18 +0800

    Update console.cpp

--- src/chimera/console/console.cpp
+++ src/chimera/console/console.cpp
@@ -493,6 +493,8 @@ namespace Chimera {
                 else {
                     if(position < static_cast<std::size_t>(half_page)) {
                         position = 0;
+                        console_output("console_text is:%s",console_text);
+                        console_output("临时文本是:%s",console_text_temp);
                     }
                     else {
                         position -= half_page;
